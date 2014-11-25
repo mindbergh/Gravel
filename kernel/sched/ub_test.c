ee03ed12af12350767fa6a3dff7ebf23b546811b
xuehung <xuehung@gmail.com>
2014-11-24 19:26:52 -0500

    add bound table

--- kernel/sched/ub_test.c
+++ kernel/sched/ub_test.c
@@ -13,7 +13,16 @@
 #include <exports.h>
 #endif
 
-
+float bound_table[OS_MAX_TASKS] = {
+    1.0000, 0.8284, 0.7798, 0.7568, 0.7435, 0.7348, 0.7286, 0.7241,
+    0.7205, 0.7177, 0.7155, 0.7136, 0.7120, 0.7106, 0.7094, 0.7084,
+    0.7075, 0.7067, 0.7059, 0.7053, 0.7047, 0.7042, 0.7037, 0.7033,
+    0.7028, 0.7025, 0.7021, 0.7018, 0.7015, 0.7012, 0.7010, 0.7007,
+    0.7005, 0.7003, 0.7001, 0.6999, 0.6997, 0.6995, 0.6993, 0.6992,
+    0.6990, 0.6989, 0.6988, 0.6986, 0.6985, 0.6984, 0.6983, 0.6982,
+    0.6981, 0.6980, 0.6979, 0.6978, 0.6977, 0.6976, 0.6975, 0.6975,
+    0.6974, 0.6973, 0.6972, 0.6972, 0.6971, 0.6970, 0.6970, 0.6969
+};
 /**
  * @brief Perform UB Test and reorder the task list.
  *
@@ -41,9 +50,8 @@ int assign_schedule(task_t** tasks, size_t num_tasks)
         dbg_printf("C = %ld, T = %ld, This util is: %x\n",tmp_task->C, tmp_task->T, this_util);
         utilization += this_util;
     }
-    float threshold = 0.693; // TODO n(2^(1/n) - 1)
     dbg_printf("Sum util = %x\n", utilization);
-    if (utilization > threshold) {        
+    if (utilization > bound_table[num_tasks]) {
         return 0;
     }
 
