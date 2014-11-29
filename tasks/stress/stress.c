0f5b43e2907508af08bd0770ab44cb89d1de861e
Ming Fang <jlsqsd@gmail.com>
2014-11-29 17:59:26 -0500

    a

--- tasks/stress/stress.c
+++ tasks/stress/stress.c
@@ -79,6 +79,7 @@ int main(int argc, char** argv)
 		tasks[i].stack_pos = (void*)(0xa2000000 + 0x10000 * i);
 		tasks[i].C = (i+1);
 		tasks[i].T = (i+1)*100;
+		tasks[i].B = 0;
 		score[i] = 0;
 	}
 	tasks[i].lambda = fun2;
@@ -86,6 +87,7 @@ int main(int argc, char** argv)
   	tasks[i].stack_pos = (void*)(0xa2000000 + 0x10000 * i);
   	tasks[i].C = 0;
 	tasks[i].T = END_TIME;
+	tasks[i].B = 0;
 	task_create(tasks, NUM_TASK);
 	argc=argc; /* remove compiler warning */
 	argv=argv; /* remove compiler warning */
